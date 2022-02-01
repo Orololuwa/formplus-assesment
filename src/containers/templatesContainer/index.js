08dd28d9e6b74ed762f0fa7e334a5c800113974b
unknown <ea@afarapartners.com>
2022-02-01 23:17:54 +0100

    templates view layout

--- src/containers/templatesContainer/index.js
+++ src/containers/templatesContainer/index.js
@@ -7,7 +7,15 @@ const Templates = () => {
         <h6 className="text-gray-700">All Templates</h6>
         <p className="text-gray-400">2000 templates</p>
       </div>
-      <div className="templates py-5">
+      <div className="templates py-8 grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-y-8 justify-items-center">
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
         <TemplateCard />
         <TemplateCard />
         <TemplateCard />
