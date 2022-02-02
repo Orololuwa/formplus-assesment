1faa22a9fc35e91b03e0936662f8a8cc689eaca0
unknown <ea@afarapartners.com>
2022-02-02 23:00:48 +0100

    template UI view with pagination

--- src/containers/templatesContainer/index.js
+++ src/containers/templatesContainer/index.js
@@ -1,40 +1,51 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import Pagination from "components/Pagination";
 import TemplateCard from "components/templateCard";
 
 const Templates = () => {
+  //Data from the store
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+  //Page Variables
+  const pageSize = 3;
+  const totalCount = 11;
+
+  //Page change functions
   const [currentPage, setCurrentPage] = useState(1);
 
   const pageChangedHandler = (page) => {
     setCurrentPage(page);
   };
 
+  //Current Page data view functions
+  const currentData = useMemo(() => {
+    const start = (currentPage - 1) * pageSize;
+    const end = start + pageSize;
+    return data.slice(start, end);
+  }, [currentPage]);
+
   return (
-    <section className="pt-2 pb-10">
-      <div className="flex justify-between text-sm pt-5">
-        <h6 className="text-gray-700">All Templates</h6>
-        <p className="text-gray-400">2000 templates</p>
-      </div>
-      <div className="templates py-8 grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-y-8 justify-items-center">
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
-      </div>
-      <Pagination
-        pageSize={3}
-        totalCount={11}
-        currentPage={currentPage}
-        onPageChange={pageChangedHandler}
-      />
-    </section>
+    <>
+      {data && (
+        <section className="pt-2 pb-10">
+          <div className="flex justify-between text-sm pt-5">
+            <h6 className="text-gray-700">All Templates</h6>
+            <p className="text-gray-400">{totalCount} templates</p>
+          </div>
+          <div className="templates py-8 grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-y-8 justify-items-center">
+            {currentData.map((temp, idx) => (
+              <TemplateCard key={idx} />
+            ))}
+          </div>
+          <Pagination
+            pageSize={pageSize}
+            totalCount={totalCount}
+            currentPage={currentPage}
+            onPageChange={pageChangedHandler}
+          />
+        </section>
+      )}
+    </>
   );
 };
 
